024695921b438826078f407e6344939f0660b4a5
Sovietmade <ch3rkas0v.yuri@gmail.com>
2014-09-16 22:06:07 +0400

    testing stuff

--- Main.cs
+++ Main.cs
@@ -1,17 +1,17 @@
-
+using System;
 //oauth:4286uj803vttts1k6reigd8snxngmjl
 namespace TwitchChatBot {
 	class Flow{
 		static void Main(){
 			TcpConnection tt = new TcpConnection();
-			tt.Proxy = new Endpoint();
-			tt.Proxy.EndpointAddress = "eproxy.volga";
-			tt.Proxy.EndpointPort = 8080;
+			//tt.Proxy = new Endpoint();
+			//tt.Proxy.EndpointAddress = "eproxy.volga";
+			//tt.Proxy.EndpointPort = 8080;
 			tt.Destination = new Endpoint();
 			tt.Destination.EndpointAddress = "irc.twitch.tv";
 			tt.Destination.EndpointPort = 6667;
 			tt.Connect();
-
+            Console.WriteLine("");
 		}
 	}
 }
--- TcpConnection.cs
+++ TcpConnection.cs
@@ -40,7 +40,7 @@ namespace TwitchChatBot
 		{
 			byte[] buffer = new byte[2048];
 			int bytes = mNetworkStream.Read(buffer, 0, buffer.Length);
-           	Console.Write(Encoding.UTF8.GetString(buffer, 0, bytes));
+           	Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, bytes));
 		}
 
 		public void Connect(){
@@ -54,10 +54,17 @@ namespace TwitchChatBot
 				string tunnelRequest = String.Format("CONNECT {0}:{1}  HTTP/1.1\r\nHost: {0}\r\n\r\n", Destination.EndpointAddress,Destination.EndpointPort);
 				SendMessage(tunnelRequest);
 				ReadMessage();
-				SendMessage("PASS oauth:4286uj803vttts1k6reigd8snxngmjl");
-				SendMessage("NICK sovietmade");
-				ReadMessage();
+
 			}
+            SendMessage("PASS oauth:lxubjjlsavkv1o3ih44d3csztfpw7vu\r\n");
+            SendMessage("NICK sovietmade\r\n");
+            ReadMessage();
+            SendMessage("USER sovietmade 0 * :lol\r\n");
+            SendMessage("JOIN #sovietmade\r\n");
+            ReadMessage();
+            SendMessage("PRIVMSG #sovietmade :test\r\n");
+            //SendMessage("JOIN sovietmade\r\n");
+            ReadMessage();
 		}
 
 		TcpClient mTcpClient;
