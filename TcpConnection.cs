290704271f1de6e2d58e63d5b2c2cc4cee89e68c
Sovietmade <ch3rkas0v.yuri@gmail.com>
2014-09-17 15:55:34 +0400

    inprogress

--- TcpConnection.cs
+++ TcpConnection.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
 
 namespace TwitchChatBot
 {
@@ -15,6 +18,11 @@ namespace TwitchChatBot
 		{
 			get;
 			set;
+		}
+
+		public override string ToString ()
+		{
+			return string.Format ("{0}:{1}", EndpointAddress, EndpointPort);
 		}
 	}
 
@@ -36,42 +44,78 @@ namespace TwitchChatBot
 			mNetworkStream.Flush();
 		}
 
-		public void ReadMessage ()
+
+		public void Send (string inMessage)
 		{
-			byte[] buffer = new byte[2048];
-			int bytes = mNetworkStream.Read(buffer, 0, buffer.Length);
-           	Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, bytes));
+			lock (MessageQ) {
+				MessageQ.Write(Encoding.UTF8.GetBytes(inMessage),0,Encoding.UTF8.GetBytes(inMessage).Length);
+			}
+			SendData(null);
 		}
 
 		public void Connect(){
 			if (Proxy == null) {
 				mTcpClient = new TcpClient(Destination.EndpointAddress,Destination.EndpointPort);
 				mNetworkStream = mTcpClient.GetStream();
+				mNetworkStream.BeginRead(Buffer,0,Buffer.Length,new AsyncCallback(DataReceived),null);
 			} 
 			else {
 				mTcpClient = new TcpClient(Proxy.EndpointAddress,Proxy.EndpointPort);
 				mNetworkStream = mTcpClient.GetStream();
-				string tunnelRequest = String.Format("CONNECT {0}:{1}  HTTP/1.1\r\nHost: {0}\r\n\r\n", Destination.EndpointAddress,Destination.EndpointPort);
+				mNetworkStream.BeginRead(Buffer,0,Buffer.Length,new AsyncCallback(DataReceived),null);
+				string tunnelRequest = String.Format("CONNECT {0}  HTTP/1.1\r\nHost: {0}\r\n\r\n", Destination.ToString());
 				SendMessage(tunnelRequest);
-				ReadMessage();
-
 			}
-			mNetworkStream.BeginRead(Buffer,0,Buffer.Length,new AsyncCallback(DataReceived),null);
-            SendMessage("PASS oauth:lxubjjlsavkv1o3ih44d3csztfpw7vu\r\n");
-            SendMessage("NICK sovietmade\r\n");
+
+
+            Send("PASS oauth:lxubjjlsavkv1o3ih44d3csztfpw7vu\r\n");
+            Send("NICK sovietmade\r\n");
             //ReadMessage();
-            SendMessage("USER sovietmade 0 * :lol\r\n");
-            SendMessage("JOIN #sovietmade\r\n");
+            
+            Send("JOIN #sovietmade\r\n");
             //ReadMessage();
-            SendMessage("PRIVMSG #sovietmade :test\r\n");
+            Send("PRIVMSG #sovietmade :test\r\n");
             //SendMessage("JOIN sovietmade\r\n");
             //ReadMessage();
 		}
 
+
+		private void SendData (IAsyncResult result)
+		{
+
+			if (result != null) {
+				mNetworkStream.EndWrite(result);
+				lock(MessageQ){
+					sending = false;
+				}
+			}
+
+			MemoryStream ms = null;
+
+			lock (MessageQ) {
+				if (sending) {
+					return;
+				}
+				if (MessageQ.Length != 0) {
+					sending = true;
+					ms = new MemoryStream();
+
+					MessageQ.CopyTo(ms);
+					MessageQ.SetLength(0);
+
+					Console.WriteLine(Encoding.UTF8.GetString(ms.ToArray()));
+				}
+			}
+
+			if (ms != null) {
+
+				mNetworkStream.BeginWrite(ms.ToArray(),0,(int)ms.Length, new AsyncCallback(SendData), null);
+			}
+		}
 		private void DataReceived( IAsyncResult result)
 		{
 			int receivedDataLength = mNetworkStream.EndRead(result);
-			Console.WriteLine("Received {0} bytes: {1}", receivedDataLength, Encoding.UTF8.GetString(Buffer));
+			Console.WriteLine("Received {0} bytes:\n {1}", receivedDataLength, Encoding.UTF8.GetString(Buffer));
 			mNetworkStream.BeginRead(Buffer,0,Buffer.Length,new AsyncCallback(DataReceived),null);
 
 		}
@@ -81,6 +125,8 @@ namespace TwitchChatBot
 		public Endpoint Proxy;
 		public Endpoint Destination;
 		byte[] Buffer = new byte[2048];
+		MemoryStream MessageQ = new MemoryStream();
+		bool sending = false;
 	}
 }
 
